3bef8323d2ba742a330b0000ec1834ebf5adf8eb
joaop21 <jpsilva9898@gmail.com>
2020-03-03 22:57:00 +0000

    Change scripts in order to run them properly

--- bin/fabric-setup/fabric-setup-part1.sh
+++ bin/fabric-setup/fabric-setup-part1.sh
@@ -1,38 +1,38 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 set -e
-. "functions"
+. "functions.sh"
 
 env=${1:-"dev"}
 
-pp_info "setup" "Installing Prerequisites"
+pp_info "setup-1" "Installing Prerequisites"
 
 if not_installed "git"; then
-  pp_error "setup" "
+  pp_error "setup-1" "
   We are using git to download the needed repositories, since it was not found on your  system we cannot ensure that you are using the correct versions of all the tools. 
   Please install it and run this script again, or proceed at your own peril.
   "
 
   ensure_confirmation
 else
-  pp_success "setup" "git is already installed"
+  pp_success "setup-1" "git is already installed"
 fi
 
 if not_installed "curl"; then
-  pp_error "setup" "
+  pp_error "setup-1" "
   We are using curl, since it was not found on your  system we cannot ensure that you   are using the correct versions of all the tools. Please install it and run this 
   script again, or proceed at your own peril.
   "
 
   ensure_confirmation
 else
-  pp_success "setup" "curl is already installed"
+  pp_success "setup-1" "curl is already installed"
 fi
 
 
 if not_installed "docker"; then
-  pp_error "setup" "We are using docker for our fabric network which isn't installed."
-  pp_info "setup" "We are installing docker"
+  pp_error "setup-1" "We are using docker for our fabric network which isn't installed."
+  pp_info "setup-1" "We are installing docker"
   sudo apt-get update
   sudo apt-get install \
     apt-transport-https \
@@ -48,38 +48,38 @@ if not_installed "docker"; then
    stable"
   sudo apt-get update
   sudo apt-get install docker-ce docker-ce-cli containerd.io
-  pp_info "setup" "Enabling Docker to start on boot."
+  pp_info "setup-1" "Enabling Docker to start on boot."
   sudo systemctl start docker
   sudo systemctl enable docker
-  sudo usermod -a -G docker $(user)
-  pp_success "setup" "docker is up-and-running"
+  sudo usermod -a -G docker $USER
+  pp_success "setup-1" "docker is up-and-running"
 else
   docker_state=$(sudo docker info >/dev/null 2>&1)
   if [[ $? -ne 0 ]]; then
-    pp_warn "setup" "docker does not seem to be running, run it first and retry"
+    pp_warn "setup-1" "docker does not seem to be running, run it first and retry"
     exit 1
   else
-    pp_success "setup" "docker is up-and-running"
+    pp_success "setup-1" "docker is up-and-running"
   fi
 fi
 
 
 if not_installed "docker-compose"; then
-  pp_error "setup" "We are using docker-compose for our fabric network which isn't installed."
-  pp_info "setup" "We are installing docker-compose"
+  pp_error "setup-1" "We are using docker-compose for our fabric network which isn't installed."
+  pp_info "setup-1" "We are installing docker-compose"
   sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
-  pp_success "setup" "docker-compose is up-and-running"
+  pp_success "setup-1" "docker-compose is up-and-running"
 else
-  pp_success "setup" "docker-compose is already installed"
+  pp_success "setup-1" "docker-compose is already installed"
 fi
 
 
-pp_info "setup" "Installing Required Languages"
+pp_info "setup-1" "Installing Required Languages"
 
 if not_installed "go"; then
-  pp_error "setup" "We are using Go as development language which isn't installed."
-  pp_info "setup" "We are installing Go."
+  pp_error "setup-1" "We are using Go as development language which isn't installed."
+  pp_info "setup-1" "We are installing Go."
   wget https://dl.google.com/go/go1.14.linux-amd64.tar.gz
   sudo tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz
   echo "export GOROOT=\"/usr/local/go\"" >> ~/.profile
@@ -89,19 +89,8 @@ if not_installed "go"; then
   touch ~/.profile
   rm go1.14.linux-amd64.tar.gz
 
-  pp_warn "setup" "You just need to exit your account and enter again."
 else
   pp_success "setup" "Go is already installed"
 fi
 
-pp_info "setup" "Install Samples, Binaries, and Docker Images"
-
-cd ..
-if [ -d "fabric-samples" ]; then
-  pp_success "setup" "Directory fabric-samples exists." 
-else
-  curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.0.0 1.4.4 0.4.18
-fi
-cd bin/
-
-pp_success "setup" "You're good to go!"
+pp_warn "setup-1" "You just need to logout and enter again. Then run ./fabric-setup-part2.sh ."
--- bin/fabric-setup/functions.sh
+++ bin/fabric-setup/functions.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 BLUE='\033[1;34m'
 GREEN='\033[1;32m'
