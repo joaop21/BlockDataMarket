eb691f55e0730e39ec71a81a40a8aafe66e340c6
joaop21 <jpsilva9898@gmail.com>
2020-03-04 14:54:07 +0000

    Get the lastest versions instead of specific ones

--- bin/fabric-setup/fabric-setup-part2.sh
+++ bin/fabric-setup/fabric-setup-part2.sh
@@ -11,7 +11,7 @@ cd ../../
 if [ -d "fabric-samples" ]; then
   pp_success "setup-2" "Directory fabric-samples exists." 
 else
-  sudo curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.0.0 1.4.4 0.4.18
+  sudo curl -sSL https://bit.ly/2ysbOFE | bash -s
 fi
 cd bin/fabric-setup/
 
