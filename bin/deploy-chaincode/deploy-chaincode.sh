9e2dcd9e1bd085d0bab1c1c742b4f8a56f81a0d1
joaop21 <jpsilva9898@gmail.com>
2020-03-19 23:29:56 +0000

    Script for chaincode deployment

---
+++ bin/deploy-chaincode/deploy-chaincode.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+set -e
+. "../functions.sh"
+
+# Print the usage message
+function printHelp() {
+  echo "Usage: "
+  echo "  deploy-chaincode.sh [Flags]"
+  echo "    Flags:"
+  echo "    -pn <package name> - defines a name for the smart contract package"
+  echo "    -h (print this message)"
+  echo
+  echo "Taking all defaults:"
+  echo "  deploy-chaincode.sh"
+}
+
+# default values
+env=${1:-"dev"}
+PACKAGE_NAME=dataMarket
+
+# parse flags
+
+while [[ $# -ge 1 ]] ; do
+  key="$1"
+  case $key in
+  -h )
+    printHelp
+    exit 0
+    ;;
+  -pn )
+    PACKAGE_NAME="$2"
+    shift
+    ;;
+  * )
+    echo
+    echo "Unknown flag: $key"
+    echo
+    printHelp
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+pp_info "Deploy" "Package the smart contract"
+cd ../../chaincode/
+go mod init $PACKAGE_NAME
+export GO111MODULE=on
+go mod vendor
+export GO111MODULE=""
+cd ../fabric-samples/test-network
+export PATH=${PWD}/../bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+peer lifecycle chaincode package $PACKAGE_NAME.tar.gz --path ../../chaincode/ --lang golang --label $PACKAGE_NAME
+
+pp_info "Deploy" "Install the chaincode package on Org1"
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+peer lifecycle chaincode install $PACKAGE_NAME.tar.gz
+
+pp_info "Deploy" "Install the chaincode package on Org2"
+CORE_PEER_LOCALMSPID="Org2MSP"
+CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+peer lifecycle chaincode install $PACKAGE_NAME.tar.gz
+
+pp_info "Deploy" "Approve chaincode definition in Org2"
+peer lifecycle chaincode queryinstalled
+echo
+echo "Copy 'Package ID' and past it here:"
+read input
+export CC_PACKAGE_ID=$input
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name $PACKAGE_NAME --version 1.0 --init-required --package-id $CC_PACKAGE_ID --sequence 1 --tls true --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+pp_info "Deploy" "Approve chaincode definition in Org1"
+CORE_PEER_LOCALMSPID="Org1MSP"
+CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+CORE_PEER_ADDRESS=localhost:7051
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name $PACKAGE_NAME --version 1.0 --init-required --package-id $CC_PACKAGE_ID --sequence 1 --tls true --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+pp_info "Deploy" "Committing the chaincode definition to the channel"
+peer lifecycle chaincode checkcommitreadiness --channelID mychannel --name $PACKAGE_NAME --version 1.0 --init-required --sequence 1 --tls true --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --output json
+peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name $PACKAGE_NAME --version 1.0 --sequence 1 --init-required --tls true --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+peer lifecycle chaincode querycommitted --channelID mychannel --name $PACKAGE_NAME --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
