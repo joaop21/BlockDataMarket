6ed9db2ec02dd24681e83d93e0f31d13d76c9b85
nelsontss <nelsontss@gmail.com>
2020-03-25 15:45:10 +0000

    fix

--- chaincode/main.go
+++ chaincode/main.go
@@ -14,31 +14,13 @@ func main() {
 	purchaseContract := new(PurchaseContract)
 	purchaseContract.Name = "PurchaseContract"
 
-	chaincode_announc, err_announc := contractapi.NewChaincode(announcementContract)
-	chaincode_ident, err_ident := contractapi.NewChaincode(identificationContract)
-	chaincode_purch, err_purch := contractapi.NewChaincode(purchaseContract)
+	chaincode, err := contractapi.NewChaincode(announcementContract, identificationContract, purchaseContract)
 
-	if err_announc != nil {
+	if err != nil {
 		panic(err.Error())
 	}
 
-	if err := chaincode_announc.Start(); err != nil {
-		panic(err.Error())
-	}
-
-	if err_ident != nil {
-		panic(err.Error())
-	}
-
-	if err := chaincode_ident.Start(); err != nil {
-		panic(err.Error())
-	}
-
-	if err_purch != nil {
-		panic(err.Error())
-	}
-
-	if err := chaincode_purch.Start(); err != nil {
+	if err := chaincode.Start(); err != nil {
 		panic(err.Error())
 	}
 }
