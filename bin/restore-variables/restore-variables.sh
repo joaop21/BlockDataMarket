264c6ecd06f1a71bb9846036d7cb4e4ea8b271fd
joaop21 <jpsilva9898@gmail.com>
2020-03-25 18:37:53 +0000

    Add needed variables

--- bin/restore-variables/restore-variables.sh
+++ bin/restore-variables/restore-variables.sh
@@ -3,8 +3,6 @@
 set -e
 . "../functions.sh"
 
-env=${1:-"dev"}
-
 pp_info "Restore Variables" "Re-Defining environment variables"
 cd ../../fabric-samples/test-network/
 export PATH=${PWD}/../bin:${PWD}:$PATH
@@ -14,4 +12,7 @@ export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export ORG2_CA=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 cd ../../bin/restore-variables/
