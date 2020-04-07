65e40735860d8106d6a0ef2c74c9d969e133393f
joaop21 <jpsilva9898@gmail.com>
2020-04-07 18:14:42 +0100

    Change comment

--- application/usage_examples.js
+++ application/usage_examples.js
@@ -5,7 +5,7 @@ mongo.putContent('./package.json').then((dataID) => {
   console.log(dataID)
 });
 
-// for putting content in db
+// for get content from db
 mongo.getContent('d7e61b40-78ef-11ea-bb13-f169cd2409f9').then((result) => {
   console.log(result)
 });
