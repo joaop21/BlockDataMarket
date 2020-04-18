be63264c1b069ecc1e4e1877e218e9072f386b2c
joaop21 <jpsilva9898@gmail.com>
2020-04-18 16:13:40 +0100

    Making concat function public

--- chaincode/utils/strings.go
+++ chaincode/utils/strings.go
@@ -4,7 +4,7 @@ import "strings"
 
 // Function for string concatenation using builder, thus not creating a new string every time.
 // Strings are immutable and it's a slow operation without builder.
-func concat(strs ...string) string {
+func Concat(strs ...string) string {
 	var sb strings.Builder
 	for _, str := range strs {
 		sb.WriteString(str)
